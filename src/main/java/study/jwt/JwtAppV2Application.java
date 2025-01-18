455767189e3c023d1258dcd4d184e89324c48334
Максим Фоминцев <max135603@gmail.com>
2025-01-19 00:01:25 +0300

    Spring boot приложение

---
+++ src/main/java/study/jwt/JwtAppV2Application.java
@@ -0,0 +1,11 @@
+package study.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAppV2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAppV2Application.class, args);
+	}
+}
