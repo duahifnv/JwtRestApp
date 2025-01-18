b87fcf83bb16da8f046793cf32e5b35cfd9e975f
Максим Фоминцев <max135603@gmail.com>
2025-01-19 00:05:35 +0300

    Классы тестирования приложения

---
+++ src/test/java/study/jwt/JwtAppV2ApplicationTests.java
@@ -0,0 +1,11 @@
+package study.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAppV2ApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
